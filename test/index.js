6a4ea7a76f8bc8e7b3397871bf372cb307e6f927
Jason Lai <jason@jasonlai.net>
2015-06-14 19:47:27 -0700

    Add some tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,17 @@
 'use strict';
 
+var CLUSTO_TYPES = require('../index').CLUSTO_TYPES;
 var ClustoClient = require('../index').ClustoClient;
 var test = require('tape');
 
 test('ClustoClient is a function', function t(assert) {
     assert.equal(typeof ClustoClient, 'function');
     assert.end();
+});
+
+test('ClustoClient includes a list of instance methods named from Clusto types', function t(assert) {
+	CLUSTO_TYPES.forEach(function eachClustoType(typeName) {
+		assert.equal(typeof ClustoClient.prototype[typeName], 'function');
+	});
+	assert.end();
 });
